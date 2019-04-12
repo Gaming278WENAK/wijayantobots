5ef61600cb76762d9bfff5316a036d1483c8b7fa
HanzProstama <47862061+Gaming278WENAK@users.noreply.github.com>
2019-04-12 14:17:02 +0700

    Update and rename dia.db to fuck.js

---
+++ fuck.js
@@ -0,0 +1,13 @@
+
+
+
+
+const fetch = require("superagent");
+      fetch.get(`https://darahkita.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
+    setInterval(() => {
+      fetch.get(`https://darahkita.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
+    }, 180000); 
+    setInterval(() => {
+      fetch.get(`https://darahkita.glitch.me/`).set('user-agent', 'official-pinger/1.0.0').then(x => x);
+    }, 240000);
+
